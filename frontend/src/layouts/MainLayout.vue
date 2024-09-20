915e0a2effb9ea48c8858386eeec3ce26bf6af52
vdmkkk <soloviov.vad@gmail.com>
2024-09-21 02:10:50 +0300

    wtf api yo

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -6,4 +6,13 @@
   </q-layout>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import useAuth from 'src/api/composables/useAuth';
+import { onMounted } from 'vue';
+
+const { apiLogin } = useAuth();
+
+onMounted(() => {
+  apiLogin('vdmk', '123');
+});
+</script>
